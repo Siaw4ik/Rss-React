e5d3a2cf1f15df8518a19e3b022162060f821f7b
Siaw4ik <peretsviach@gmail.com>
2023-03-21 21:55:58 +0100

    feat: create basic structure form page

--- react-components/src/pages/FormsPage.tsx
+++ react-components/src/pages/FormsPage.tsx
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 
 export class FormsPage extends Component {
   render() {
-    return <div className="container_form">page form</div>;
+    return (
+      <div className="container_formpage">
+        <div className="container_form-block">
+          <h3>Form for creating and adding a product</h3>
+          <div className="wrapper_form"></div>
+        </div>
+        <div className="container_createdCrads">
+          <h3>Created and added products</h3>
+          <div className="wrapper_cardList"></div>
+        </div>
+      </div>
+    );
   }
 }
