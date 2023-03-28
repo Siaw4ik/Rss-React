d9648b024d96c70525bb1a975b3a396d8b5b72f8
Siaw4ik <peretsviach@gmail.com>
2023-03-29 01:38:31 +0200

    refactor: rewrite FormsPage Components

--- react-components/src/pages/FormsPage.tsx
+++ react-components/src/pages/FormsPage.tsx
@@ -1,9 +1,9 @@
-import { ProductForm, FormPageProps, FormPageState } from '../date/types_date';
-import React, { Component } from 'react';
+import { ProductForm } from '../date/types_date';
+import React, { useState } from 'react';
 import { Form } from '../components/Form';
 import { CardForm } from '../components/CardForm';
 
-export class FormsPage extends Component<FormPageProps, FormPageState> {
+/* export class FormsPage extends Component<FormPageProps, FormPageState> {
   constructor(props: FormPageProps) {
     super(props);
 
@@ -46,4 +46,38 @@ export class FormsPage extends Component<FormPageProps, FormPageState> {
       </div>
     );
   }
+} */
+
+export function FormsPage() {
+  const [products, setProducts] = useState<ProductForm[]>([]);
+  const [isActive, setIsActive] = useState(false);
+
+  const addProduct = (product: ProductForm) => {
+    setProducts((prevProducts) => [...prevProducts, product]);
+  };
+
+  const showModalWindow = () => {
+    setIsActive(!isActive);
+  };
+
+  const toggleClass = isActive ? ' active' : '';
+  return (
+    <div className="container_formpage">
+      <div className="container_form-block">
+        <h3 data-testid="formpage-formtitle">Form for creating and adding a product</h3>
+        <Form addProduct={addProduct} showModalWindow={showModalWindow} />
+      </div>
+      <div className="container_createdCrads">
+        <h3 data-testid="formpage-cardsformtitle">Created and added products</h3>
+        <div className="wrapper_cardList">
+          {products.map((product, index) => (
+            <CardForm key={index} product={product} />
+          ))}
+        </div>
+      </div>
+      <div className={`modalWindow${toggleClass}`} data-testid="show-modal-button">
+        <p>Data saved, card created</p>
+      </div>
+    </div>
+  );
 }
