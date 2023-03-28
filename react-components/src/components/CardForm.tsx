43debda5526ceee94dc2f9ea26b63579e5d49368
Siaw4ik <peretsviach@gmail.com>
2023-03-29 01:54:33 +0200

    refactor: rewrite CardForm Components

--- react-components/src/components/CardForm.tsx
+++ react-components/src/components/CardForm.tsx
@@ -1,31 +1,25 @@
-import { CardFormProps, CardState } from 'date/types_date';
-import React, { Component } from 'react';
+import { CardFormProps } from 'date/types_date';
+import React from 'react';
 
-export class CardForm extends Component<CardFormProps, CardState> {
-  constructor(props: CardFormProps) {
-    super(props);
-  }
-  render() {
-    const product = this.props.product;
-    return (
-      <div data-testid="card" className="cardForm">
-        <div className="card-descriptionForm">
-          <img className="image-productForm" src={product.image} alt="image product" />
-          <div className="total-description-productForm">
-            <h3 className="title-productForm">{product.title}</h3>
-            <p className="description-productForm">{product.description}</p>
-            <span className="category-productForm">{product.category}</span>
-            <div className="rating-productForm">
-              <p className="count-productForm">Count: {product.count}</p>
-              <p className="price-productForm">Price: ${product.price}</p>
-            </div>
-            <div className="availability-date_productForm">
-              <span className="availability-productForm">{product.presence}</span>
-              <span className="date-productForm">{product.date}</span>
-            </div>
+export function CardForm(props: CardFormProps) {
+  return (
+    <div data-testid="card" className="cardForm">
+      <div className="card-descriptionForm">
+        <img className="image-productForm" src={props.product.image} alt="image product" />
+        <div className="total-description-productForm">
+          <h3 className="title-productForm">{props.product.title}</h3>
+          <p className="description-productForm">{props.product.description}</p>
+          <span className="category-productForm">{props.product.category}</span>
+          <div className="rating-productForm">
+            <p className="count-productForm">Count: {props.product.count}</p>
+            <p className="price-productForm">Price: ${props.product.price}</p>
+          </div>
+          <div className="availability-date_productForm">
+            <span className="availability-productForm">{props.product.presence}</span>
+            <span className="date-productForm">{props.product.date}</span>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
